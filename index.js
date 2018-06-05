2986324fa7ce3eab9cc49896be96af1ac3c1d83d
Drew Rollins <drewrollins14@gmail.com>
2018-06-05 18:39:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("div#nested div div div div.target");
+}
+
+function increaseRankBy(n) {
+  var change = document.querySelectorAll("ul.ranked-list");
+  
+  for(var i = 0; i < change.length; i++) {
+    change[i].innerHTML = parseInt(change[i].innerHTML);
+  }
+}
+
+function deepestChild() {
+  var deeper = document.querySelectorAll("#grand-node div div div div");
+  for(var i = 0; i < deeper.length; i++) {
+    return deeper[i];
+  }
+}

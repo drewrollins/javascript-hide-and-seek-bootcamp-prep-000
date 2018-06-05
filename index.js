0f7c6044a3d43735841f28e80336b409b38165a6
Drew Rollins <drewrollins14@gmail.com>
2018-06-05 18:11:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("div#nested div div div div.target");
+}
+
+function increaseRankBy(n) {
+  var change = document.querySelectorAll("ul.ranked-list");
+  
+  for(var i = 0; i < change.length; i++) {
+    change[i].innerHTML = parseInt(change[i].innerHTML);
+  }
+}
+
+function deepestChild() {
+  var deeper = document.querySelectorAll("div#grand-node");
+}
